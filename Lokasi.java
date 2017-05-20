efbe4321c074eae187917185dc24e141e736d36e
FajarBaihaqy <fajar.sumbergedong@gmail.com>
2017-05-21 00:32:06 +0700

    Add files via upload

--- Lokasi.java
+++ Lokasi.java
@@ -1,55 +1,93 @@
 package Project;
 
+import java.util.Scanner;
+
 public class Lokasi {
-	String Kota;
-	String Kec;
-	String Desa;
-	String RT;
+	String Nama;
+    String JenisKelamin;
+    String Kota;
+    String Kec;
+    String Desa;
+    String RT;
+
+    public Lokasi(){
 	
-	public Lokasi(){
-		
-	}
-	public Lokasi(String kota, String kec, String desa, String rT) {
-		super();
+    }
+    
+    public Lokasi(String nama, String jenisKelamin, String kota, String kec, String desa, String rT) {
+    	super();
+        Nama = nama;
+        JenisKelamin = jenisKelamin;
+        Kota = kota;
+        Kec = kec;
+        Desa = desa;
+        RT = rT;
+    }
+    
+    public Lokasi(String kota, String kec, String desa, String rT) {
+	super();
 		Kota = kota;
 		Kec = kec;
 		Desa = desa;
 		RT = rT;
-	}
-	public Lokasi(String kota, String kec, String desa) {
-		super();
+    }
+    public Lokasi(String kota, String kec, String desa) {
+    	super();
 		Kota = kota;
 		Kec = kec;
 		Desa = desa;
-	}
-	public Lokasi(String desa, String rT) {
-		super();
-		Desa = desa;
-		RT = rT;
-	}
-	
-	public String getKota() {
-		return Kota;
-	}
-	public String getKec() {
-		return Kec;
-	}
-	public String getDesa() {
-		return Desa;
-	}
-	public String getRT() {
-		return RT;
-	}
-	public void setKota(String kota) {
-		Kota = kota;
-	}
-	public void setKec(String kec) {
-		Kec = kec;
-	}
-	public void setDesa(String desa) {
-		Desa = desa;
-	}
-	public void setRT(String rT) {
-		RT = rT;
-	}
+    }
+    public Lokasi(String desa, String rT) {
+    	super();
+    	Desa = desa;
+    	RT = rT;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public String getJenisKelamin() {
+        return JenisKelamin;
+    }
+
+    
+    
+    public String getKota() {
+    	return Kota;
+    }
+    public String getKec() {
+    	return Kec;
+    }
+    public String getDesa() {
+    	return Desa;
+    }
+    public String getRT() {
+    	return RT;
+    }
+    
+    public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+
+    public void setJenisKelamin(String JenisKelamin) {
+    	this.JenisKelamin = JenisKelamin;
+    }
+    
+    
+    
+    public void setKota(String kota) {
+    	Kota = kota;
+    }
+    public void setKec(String kec) {
+    	Kec = kec;
+    }
+    public void setDesa(String desa) {
+    	Desa = desa;
+    }
+    public void setRT(String rT) {
+    	RT = rT;
+    }
+
+
 }
--- theRun.java
+++ theRun.java
@@ -1,15 +1,27 @@
 package Project;
-import java.util.ArrayList;
+
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class theRun {
 	public static void main(String[] args) {
-	Lokasi L = new Lokasi();
-	HashMap<String, Lokasi> con = new HashMap<>();
-	con.put("Lowokwaru", L.get);
+		Lokasi L = new Lokasi();
+		HashMap<String, Lokasi> con = new HashMap<>();
+		
+	        con.put("Wagiran", new Lokasi("Wagiran", "Laki-laki", "Malang", "Karangploso", "Tegalgondo", "11"));
+	        con.put("Paniran", new Lokasi("Paniran", "Laki-laki", "Malang", "Karangploso", "Tegalgondo", "9"));
+	        con.put("Sri", new Lokasi("Sri", "Perempuan", "Malang", "Karangploso", "Tegalgondo", "6"));
+	        con.put("Sri", new Lokasi("Sri", "Perempuan", "Malang", "Karangploso", "Tegalgondo", "6"));
+	        
+	        
+	      
+	        con.forEach((k,v) -> System.out.println("key: "+k+" - "+v.getNama()+" --> "+v.getJenisKelamin()+" --> "+v.getKota()+" --> "+v.getKec()+" --> "+v.getDesa()+" --> "+v.getRT()));
+		
 
-	con.put("Tlogomas1", new Lokasi("Tlogomas", "01"));
-	con.put("Tlogomas1", new Lokasi("Tlogomas", "01"));
-	}
+	        Scanner scan = new Scanner(System.in);  
+	        System.out.println("Masukkan nama : ");  
+	        //String value = L.get();  
+	        //System.out.println("Data" + value); 
+	        }
 	
 }
